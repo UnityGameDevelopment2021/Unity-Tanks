4d401e537f0eaea7a198154c0793be1686b59d66
bbeldycki <beldyckibartosz@gmail.com>
2021-07-08 23:19:05 +0200

    Tank + shooting mechanic
    Add tank prefabs with all scripts and buller prefab with scripts.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float shellSpeed = 10f;
+    public int shellDamage = 5;
+    public float shellRange = 10f;
+
+    private Vector2 startPosition;
+    private float passedDistance = 0f;
+    private Rigidbody2D rigid2D;
+
+    private void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        passedDistance = Vector2.Distance(transform.position, startPosition);
+        if (passedDistance >= shellRange)
+        {
+            // DisableObject();
+            Destroy(gameObject);
+        }
+    }
+
+    private void DisableObject()
+    {
+        rigid2D.velocity = Vector2.zero;
+        gameObject.SetActive(false);
+    }
+
+    public void Initialize()
+    {
+        startPosition = transform.position;
+        Debug.Log(rigid2D.velocity);
+        rigid2D.velocity = transform.up * shellSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+
+}
