807ddbbd54d63d563f895e926ed72c62a8855576
dasmitian <dasmitian@gmail.com>
2021-07-11 15:12:19 +0200

    Added rest of environment.

--- Assets/Scripts/WoodenFence.cs
+++ Assets/Scripts/WoodenFence.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BrickWall : Wall
+public class WoodenFence : Wall
 {
-
     // Start is called before the first frame update
     void Start()
     {
-        wallHP = 3;
+        wallHP = 1;
         boxCollider = GetComponent<BoxCollider2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprites[wallHP];
     }
 
-    /* TODO change collison object name
-     */ 
-    private void OnCollisionEnter2D(Collision2D collision) {
 
-        if(collision.gameObject.name == "Bullet(Clone)") {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Bullet(Clone)") {
             Bullet bullet = collision.gameObject.GetComponent<Bullet>();
             wallHP -= bullet.shellDamage;
             if (wallHP <= 0) {
@@ -29,6 +24,9 @@ public class BrickWall : Wall
                 spriteRenderer.sprite = sprite;
             }
         }
-    }
 
+        if(collision.gameObject.name == "Tank") {
+            Destroy(gameObject);
+        }
+    }
 }
